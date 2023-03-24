106e8598790e67126ac4700cd515ae11d7587e1b
Pedro Figueiredo <pedrofigsesteves@gmail.com>
2023-03-24 10:53:46 -0300

    Form estruturado

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';

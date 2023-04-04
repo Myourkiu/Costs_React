e86edfc43bd35555995702df7638286129b7c9bb
Myourkiu <pedrofigsesteves@gmail.com>
2023-04-04 14:33:14 -0300

    método de get e página criada

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
-import LinkButton from '../layout/LinkbButton'
+import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return (
--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,25 +1,57 @@
-import Message from '../layout/Message'
-import styles from './Projects.module.css'
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from "../project/ProjectCard";
 
-import { useLocation } from 'react-router-dom'
+import styles from "./Projects.module.css";
+
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 
 function Projects() {
 
-    const location = useLocation()
-    let message = ''
-    if(location.state){
-        message = location.state.message
-    }
-    console.log(location)
+  const [projects, setProjects] = useState([])
 
+  const location = useLocation();
+  let message = "";
+  if (location.state) {
+    message = location.state.message;
+  }
 
-    return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message msg={message} type="success"/>}
-        </div>
-    )
+  useEffect(() => {
+
+    fetch('http://localhost:5000/projects', {
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {setProjects(data) 
+      console.log(data)})
+    .catch((err) => console.log(err))
+  },[])
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to='/newproject' text='Criar Projeto'/>
+      </div>
+      {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+         projects.map((project) => (
+          <ProjectCard 
+          name={project.name}
+          budget={project.budget}
+          category={project.category.name}
+          key={project.id}/>
+         ))}
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
